7441d7366e216e5504bb73d926889e51715239f6
Carmen Wick <CarmenWick@gmail.com>
2011-10-07 16:22:43 -0700

    Add bootstrap.sh

---
+++ config/puppet/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd
+apt-get install git puppet
+git clone git://github.com/cwick/hello_fabric.git
+rm -rf /etc/puppet/modules/*
+rm -rf /etc/puppet/manifests/*
+cd hello_fabric/config
+cp -R puppet/modules/* /etc/puppet/modules
+puppet apply --verbose puppet/manifests/provision.pp
