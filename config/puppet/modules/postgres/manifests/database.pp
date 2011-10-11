8bb6b4fb76c156ecccc8e39333f3aa2ffd1cfbf8
Carmen Wick <CarmenWick@gmail.com>
2011-10-11 13:14:29 -0700

    Use new postgres module

--- config/puppet/modules/postgres/manifests/database.pp
+++ config/puppet/modules/postgres/manifests/database.pp
@@ -1,41 +1,6 @@
 # Based on work by Luke Kanies, luke@madstop.com
 # https://github.com/lak/puppet-postgres
 
-define postgres::role($ensure, $password = false, $superuser = false, $login = true) {
-    $passtext = $password ? {
-        false => "",
-        default => "PASSWORD '$password'"
-    }
-    $supertext = $superuser ? {
-        false => "",
-        default => "SUPERUSER"
-    }
-    $logintext = $login ? {
-        false => "NOLOGIN",
-        default => "LOGIN"
-    }
-
-    case $ensure {
-        present: {
-            exec { "Create $name postgres role":
-                command => "/usr/bin/psql -c \"CREATE ROLE $name WITH $passtext $supertext $logintext\"",
-                user => "postgres",
-                unless => "/usr/bin/psql -c '\\du' | grep '^  *$name  *|'"
-            }
-        }
-        absent:  {
-            exec { "Remove $name postgres role":
-                command => "/usr/bin/dropuser $name",
-                user => "postgres",
-                onlyif => "/usr/bin/psql -c '\\du' | grep '$name  *|'"
-            }
-        }
-        default: {
-            fail "Invalid 'ensure' value '$ensure' for postgres::role"
-        }
-    }
-}
-
 define postgres::database($ensure) {
     case $ensure {
         present: {
--- config/puppet/modules/postgres/manifests/role.pp
+++ config/puppet/modules/postgres/manifests/role.pp
@@ -35,26 +35,3 @@ define postgres::role($ensure, $password = false, $superuser = false, $login = t
         }
     }
 }
-
-define postgres::database($ensure) {
-    case $ensure {
-        present: {
-            exec { "Create $name postgres db":
-                command => "/usr/bin/createdb $name",
-                user => "postgres",
-                unless => "/usr/bin/psql -l | grep '$name  *|'"
-            }
-        }
-        absent:  {
-            exec { "Remove $name postgres db":
-                command => "/usr/bin/drop $name",
-                onlyif => "/usr/bin/psql -l | grep '$name  *|'",
-                user => "postgres"
-            }
-        }
-        default: {
-            fail "Invalid 'ensure' value '$ensure' for postgres::database"
-        }
-    }
-}
-
